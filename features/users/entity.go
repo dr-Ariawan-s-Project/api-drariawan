a025040e5cd3e99f4e774d948dd47a3ee595438e
rachmankamil <rachmankamil24@gmail.com>
2023-08-19 17:19:49 +0700

    update entity

--- features/users/entity.go
+++ features/users/entity.go
@@ -3,22 +3,23 @@ package users
 import "time"
 
 type Users struct {
-	ID         int
-	Name       string
-	Email      string
-	Password   string
-	Role       string
-	Picture    string
-	Status     string
-	Created_at time.Time
-	Updated_at time.Time
+	ID             int
+	FullName       string
+	Email          string
+	Password       string
+	Role           string
+	Picture        string
+	Specialization string
+	Status         string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
 }
 
-type UserRepositories interface {
-	Insert(data Users) (Users, error)
-	Update(data Users, id int) (Users, error)
+type Repositories interface {
+	Insert(data Users) (*Users, error)
+	Update(data Users, id int) (*Users, error)
 	Delete(id int) error
-	Select(search string, rp int, page int) ([]Users, error)
-	GetByID(id int) (Users, error)
-	GetByUsername(username string) (Users, error)
+	Select(search string, rp int, page int) ([]*Users, error)
+	GetByID(id int) (*Users, error)
+	GetByUsername(username string) (*Users, error)
 }
