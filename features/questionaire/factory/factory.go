d22841c621fa101d8ac4b4b5294405c05446a3e2
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2023-09-15 22:44:25 +0700

    feat(answer): add func post answer handler

--- features/questionaire/factory/factory.go
+++ features/questionaire/factory/factory.go
@@ -1,15 +1,16 @@
 package factory
 
 import (
+	"github.com/dr-ariawan-s-project/api-drariawan/app/config"
 	_questionaireData "github.com/dr-ariawan-s-project/api-drariawan/features/questionaire/data"
 	_questionaireHandler "github.com/dr-ariawan-s-project/api-drariawan/features/questionaire/handler"
 	_questionaireService "github.com/dr-ariawan-s-project/api-drariawan/features/questionaire/service"
 	"gorm.io/gorm"
 )
 
-func New(db *gorm.DB) *_questionaireHandler.QuestionaireHandler {
+func New(db *gorm.DB, cfg *config.AppConfig) *_questionaireHandler.QuestionaireHandler {
 	repo := _questionaireData.New(db)
-	service := _questionaireService.New(repo)
+	service := _questionaireService.New(repo, cfg)
 	handler := _questionaireHandler.New(service)
 
 	return handler
