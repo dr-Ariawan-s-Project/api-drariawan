14d1408611f6b59cfd8dacc192419cf34aaa5f84
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2023-10-16 07:53:57 +0700

    feat: change email template html

--- utils/helpers/sendEmail.go
+++ utils/helpers/sendEmail.go
@@ -1,54 +1,105 @@
 package helpers
 
 import (
+	"bytes"
 	"errors"
 	"fmt"
+	"html/template"
 	"log"
 	"net/smtp"
+	"os"
 	"strings"
 
 	"github.com/dr-ariawan-s-project/api-drariawan/app/config"
 )
 
+// Request struct
+type EmailRequest struct {
+	from    string
+	to      []string
+	subject string
+	body    string
+}
+
+func NewRequest(to []string, subject, body string) *EmailRequest {
+	return &EmailRequest{
+		to:      to,
+		subject: subject,
+		body:    body,
+	}
+}
+
 func SendMailQuestionerLink(email, codeAttempt, gmailPass string) {
 	var baseUrlFrontend = config.InitConfig().BASE_URL_FE
-	var subjectEmail = "Link Questioner dr. Ariawan App"
-	var message = `
-Terima kasih telah melakukan pendaftaran.
-Berikut adalah link pengisian questioner anda:
-`
-	message = message + baseUrlFrontend + "/kuisioner?code=" + codeAttempt
+	// Working Directory
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	errSendEmail := sendMail(email, subjectEmail, message, gmailPass)
-	if errSendEmail == nil {
-		log.Println("success send email '" + subjectEmail + "' to " + email)
+	var subjectEmail = "Link Questioner dr. Ariawan App"
+	r := NewRequest([]string{email}, subjectEmail, "-")
+
+	templateData := struct {
+		UrlLink string
+	}{
+		UrlLink: baseUrlFrontend + "/kuisioner?code=" + codeAttempt,
+	}
+	// 	var message = `
+	// Terima kasih telah melakukan pendaftaran.
+	// Berikut adalah link pengisian questioner anda:
+	// `
+	// 	message = message + baseUrlFrontend + "/kuisioner?code=" + codeAttempt
+	errTemplate := r.ParseEmailTemplate(wd+"/utils/files/index.html", templateData)
+	if errTemplate == nil {
+		errSendEmail := r.SendMail(gmailPass)
+		if errSendEmail == nil {
+			log.Println("success send email '" + subjectEmail + "' to " + email)
+		} else {
+			log.Println("error send email to "+email+". ", errSendEmail)
+		}
 	} else {
-		log.Println("error send email to "+email+". ", errSendEmail)
+		log.Println("error load template", errTemplate)
 	}
 
 }
 
-func sendMail(email, subject, message, gmailPass string) error {
+func (r *EmailRequest) ParseEmailTemplate(templateFileName string, data interface{}) error {
+	t, err := template.ParseFiles(templateFileName)
+	if err != nil {
+		return err
+	}
+	buf := new(bytes.Buffer)
+	if err = t.Execute(buf, data); err != nil {
+		return err
+	}
+	r.body = buf.String()
+	return nil
+}
+
+func (r *EmailRequest) SendMail(gmailPass string) error {
 	const CONFIG_SMTP_HOST = "smtp.gmail.com"
 	const CONFIG_SMTP_PORT = 587
 	const CONFIG_SENDER_NAME = "dr. Ariawan App <drariawan.app@gmail.com>"
 	const CONFIG_AUTH_EMAIL = "drariawan.app@gmail.com"
 	var CONFIG_AUTH_PASSWORD = gmailPass
 
-	if subject == "" || email == "" {
-		return errors.New("subject and email must be fill")
+	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
+
+	if r.subject == "" {
+		return errors.New("subject must be fill")
 	}
 
-	var to = []string{email}
+	var to = r.to
 	body := "From: " + CONFIG_SENDER_NAME + "\n" +
 		"To: " + strings.Join(to, ",") + "\n" +
-		"Subject: " + subject + "\n\n" +
-		message
+		"Subject: " + r.subject + "\n\n" +
+		mime + "\n" + r.body
 
 	auth := smtp.PlainAuth("", CONFIG_AUTH_EMAIL, CONFIG_AUTH_PASSWORD, CONFIG_SMTP_HOST)
 	smtpAddr := fmt.Sprintf("%s:%d", CONFIG_SMTP_HOST, CONFIG_SMTP_PORT)
 
-	err := smtp.SendMail(smtpAddr, auth, CONFIG_AUTH_EMAIL, append(to), []byte(body))
+	err := smtp.SendMail(smtpAddr, auth, CONFIG_AUTH_EMAIL, r.to, []byte(body))
 	if err != nil {
 		return err
 	}
